1e1b5e5c61b8f4e96688c971b31be57efdb63c9b
Michelle Mairet <michelle.rk.mairet@gmail.com>
2024-08-28 15:10:56 +0200

    Corrections of pom.xml, and implements getters setters constructors for entity classes

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,15 +1,98 @@
 package com.nnk.springboot.domain;
 
-import org.hibernate.validator.constraints.Length;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+	// TODO: Map columns in data table CURVEPOINT with corresponding java fields
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	@NotBlank(message = "Curve ID is mandatory")
+	private Integer curveId;
+
+	private Timestamp asOfDate;
+
+	private Double term;
+
+	private Double value;
+
+	private Timestamp creationDate;
+
+	// GETTERS AND SETTERS
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getCurveId() {
+		return curveId;
+	}
+
+	public void setCurveId(Integer curveId) {
+		this.curveId = curveId;
+	}
+
+	public Timestamp getAsOfDate() {
+		return asOfDate;
+	}
+
+	public void setAsOfDate(Timestamp asOfDate) {
+		this.asOfDate = asOfDate;
+	}
+
+	public Double getTerm() {
+		return term;
+	}
+
+	public void setTerm(Double term) {
+		this.term = term;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public Timestamp getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Timestamp creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	// No args constructor
+	public CurvePoint() {
+		super();
+	}
+
+	// All args constructor
+	public CurvePoint(Integer id, @NotBlank(message = "Curve ID is mandatory") Integer curveId, Timestamp asOfDate,
+			Double term, Double value, Timestamp creationDate) {
+		super();
+		this.id = id;
+		this.curveId = curveId;
+		this.asOfDate = asOfDate;
+		this.term = term;
+		this.value = value;
+		this.creationDate = creationDate;
+	}
+
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -1,12 +1,90 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.sql.Timestamp;
 
 @Entity
 @Table(name = "rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+	// TODO: Map columns in data table RATING with corresponding java fields
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	@NotBlank(message = "Moody's Rating is mandatory")
+	private String moodysRating;
+
+	@NotBlank(message = "S & P Rating is mandatory")
+	private String sandPRating;
+
+	@NotBlank(message = "Fitch Rating is mandatory")
+	private String fitchRating;
+
+	@NotBlank(message = "Order number is mandatory")
+	private Integer orderNumber;
+
+	// GETTERS AND SETTERS
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getMoodysRating() {
+		return moodysRating;
+	}
+
+	public void setMoodysRating(String moodysRating) {
+		this.moodysRating = moodysRating;
+	}
+
+	public String getSandPRating() {
+		return sandPRating;
+	}
+
+	public void setSandPRating(String sandPRating) {
+		this.sandPRating = sandPRating;
+	}
+
+	public String getFitchRating() {
+		return fitchRating;
+	}
+
+	public void setFitchRating(String fitchRating) {
+		this.fitchRating = fitchRating;
+	}
+
+	public Integer getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(Integer orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	// No args constructor
+	public Rating() {
+		super();
+	}
+
+	// All args constructor
+	public Rating(Integer id, @NotBlank(message = "Moody's Rating is mandatory") String moodysRating,
+			@NotBlank(message = "S & P Rating is mandatory") String sandPRating,
+			@NotBlank(message = "Fitch Rating is mandatory") String fitchRating,
+			@NotBlank(message = "Order number is mandatory") Integer orderNumber) {
+		super();
+		this.id = id;
+		this.moodysRating = moodysRating;
+		this.sandPRating = sandPRating;
+		this.fitchRating = fitchRating;
+		this.orderNumber = orderNumber;
+	}
+
 }
